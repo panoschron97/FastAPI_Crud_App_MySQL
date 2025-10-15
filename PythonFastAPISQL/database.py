9afeaff1f32c8461b1ba66f9cd242ba1a134f867
Panagiotis Chronopoulos <panos_chron@hotmail.com>
2025-10-15 14:34:25 +0300

    Delete PythonFastAPISQL directory

--- PythonFastAPISQL/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-db_url = "mysql://root:27031997@localhost:3306/application"
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
